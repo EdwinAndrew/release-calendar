1cb34092f56224aadc27b6dfa9c5c52a59831c21
Edwin Andrew <edwinandrew96@gmail.com>
2025-10-04 20:23:42 -0400

    Initialize Spring Boot project structure

---
+++ phase2-springboot/src/test/java/com/edwin/release_calendar/ReleaseCalendarApplicationTests.java
@@ -0,0 +1,13 @@
+package test.java.com.edwin.release_calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReleaseCalendarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
